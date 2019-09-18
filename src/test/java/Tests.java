45622faeab6dcacf66765a6c8f82eeba8ad22240
andreas0992 <andreas0991@yahoo.com>
2019-09-18 16:54:01 +0300

    tried parallel with junit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,15 +1,23 @@
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Properties;
-
+@RunWith(Parallelized.class)
 public class Tests {
+    String platform;
+    String browserName;
+    String browserVersion;
 
 
     WebDriver driver;
@@ -17,6 +25,20 @@ public class Tests {
     PetClinicHomepage petClinicHomepage;
     HeaderPage headerPage;
     OwnersPage ownersPage;
+
+    @Parameterized.Parameters
+    public static LinkedList<String[]> getEnvironments() throws Exception{
+        LinkedList<String[]> env = new LinkedList<String[]>();
+        env.add(new String[]{Platform.WIN10.toString(),"chrome","latest"});
+        env.add(new String[]{Platform.WIN10.toString(),"firefox","latest"});
+        return env;
+    }
+
+    public Tests(String platform, String browserName, String browserVersion){
+        this.platform = platform;
+        this.browserName = browserName;
+        this.browserVersion = browserVersion;
+    }
 
     @BeforeClass
     public static void initialSetUp(){
@@ -34,6 +56,7 @@ public class Tests {
     @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/config/chromedriver.exe");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(prop.getProperty("homepage"));
