5713f3f7302ab68da549243e9dc3adb23944730b
andreas0992 <andreas0991@yahoo.com>
2019-09-20 15:19:03 +0300

    multiple environments

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,15 @@
+import com.beust.jcommander.Parameter;
 import dataGeneration.OwnerGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import pages.HeaderPage;
 import pages.NewOwnerPage;
 import pages.OwnersPage;
@@ -16,6 +21,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class BaseTest {
+    Logger logger;
     Properties prop;
     WebDriver driver;
     PetClinicHomepage petClinicHomepage;
@@ -23,8 +29,9 @@ public class BaseTest {
     OwnersPage ownersPage;
     NewOwnerPage newOwnerPage;
     OwnerGenerator generatedOwner;
+
     @BeforeClass
-    public void initialSetUp(){
+    public void initialSetUp() {
         prop = new Properties();
         try {
             FileInputStream fInput = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/config/config.properties");
@@ -37,14 +44,21 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void baseSetUp(){
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/main/resources/config/chromedriver.exe");
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void baseSetUp(String browser) {
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + prop.getProperty("pathToChromeDriver"));
+            driver = new ChromeDriver();
+        }
+        else if(browser.equals("firefox")){
+            System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+ prop.getProperty("pathToFirefoxDriver"));
+            driver = new FirefoxDriver();
+        }
         driver.manage().window().maximize();
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         System.out.println("tear down");
         driver.quit();
     }
--- src/test/java/OwnersTests.java
+++ src/test/java/OwnersTests.java
@@ -1,5 +1,7 @@
 import dataGeneration.OwnerGenerator;
+import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,16 +17,26 @@ public class OwnersTests extends BaseTest {
 
     @BeforeMethod
     public void setUp() {
+        logger = LogManager.getLogger(OwnersTests.class);
         driver.get(prop.getProperty("ownersPage"));
         ownersPage = new OwnersPage(driver);
         newOwnerPage = new NewOwnerPage(driver);
     }
 
     @Test
-    public void navigateToAddOwner() throws InterruptedException, SQLException {
-        Thread.sleep(2500);
-        ownersPage.clickAddOwner();
+    public void navigateToAddOwner() {
+        logger.info("Testing the navigation to New Owner");
+        boolean addOwnerClickable = false;
+        while (!addOwnerClickable) {
+            try {
+                ownersPage.clickAddOwner();
+                addOwnerClickable = true;
+            } catch (Exception e) {
+                System.out.println("still not clickable");
+            }
+        }
         Assert.assertEquals("New Owner", newOwnerPage.getNewOwnerPageTitle().getText());
+        logger.info("New Owner".equals(newOwnerPage.getNewOwnerPageTitle().getText()) ? "PASS" : "FAIL");
     }
 
     @Test
@@ -32,13 +44,13 @@ public class OwnersTests extends BaseTest {
         Thread.sleep(2000);
         ownersPage.clickAddOwner();
         generatedOwner = new OwnerGenerator();
-//        System.out.println(generatedOwner.getFirstName() + " " + generatedOwner.getLastName() + " "
-//                + generatedOwner.getAddress() + " " + generatedOwner.getCity() + " " + generatedOwner.getPhoneNumber());
+        System.out.println(generatedOwner.getFirstName() + " " + generatedOwner.getLastName() + " "
+                + generatedOwner.getAddress() + " " + generatedOwner.getCity() + " " + generatedOwner.getPhoneNumber());
         newOwnerPage.inputFirstName(generatedOwner.getFirstName());
         newOwnerPage.inputLastName(generatedOwner.getLastName());
         newOwnerPage.inputAddress(generatedOwner.getAddress());
         newOwnerPage.inputCity(generatedOwner.getCity());
-        newOwnerPage.inputPhoneNumber(generatedOwner.getPhoneNumber());
+        newOwnerPage.inputPhoneNumber(generatedOwner.getPhoneNumber().substring(0, 10));
         newOwnerPage.clickAddOwnerButton();
         Thread.sleep(2000);
         boolean isUserPresent = false;
@@ -51,11 +63,12 @@ public class OwnersTests extends BaseTest {
         DatabaseInteraction databaseInteraction = new DatabaseInteraction();
         ArrayList<String> rows = databaseInteraction.getQueryResults(prop.getProperty("databaseUrl"), prop.getProperty("databaseUser")
                 , prop.getProperty("databasePass"), "select * from owners where first_name=\""
-                        +generatedOwner.getFirstName()+"\" and last_name=\""+generatedOwner.getLastName()+"\"");
+                        + generatedOwner.getFirstName() + "\" and last_name=\"" + generatedOwner.getLastName() + "\"");
         boolean isUserInDatabase = false;
         for (String row : rows) {
-            if(row.contains(generatedOwner.getFirstName())&&row.contains(generatedOwner.getLastName())
-                    &&row.contains(generatedOwner.getAddress()))
+            System.out.println(row);
+            if (row.contains(generatedOwner.getFirstName()) && row.contains(generatedOwner.getLastName())
+                    && row.contains(generatedOwner.getAddress()))
                 isUserInDatabase = true;
         }
         Assert.assertTrue(isUserInDatabase);
